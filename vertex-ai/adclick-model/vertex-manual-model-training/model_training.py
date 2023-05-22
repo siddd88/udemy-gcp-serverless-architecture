43335738004d5e6664216114f57fd93e3974b9fb
siddharth raghunath <siddharthraghunath794@gmail.com>
2023-05-22 10:51:04 +0400

    Update model_training.py

--- vertex-ai/adclick-model/vertex-manual-model-training/model_training.py
+++ vertex-ai/adclick-model/vertex-manual-model-training/model_training.py
@@ -9,8 +9,19 @@ bucket = storage_client.bucket("{your-bucket-name}")
 
 df = pd.read_csv('gs://{your-bucket-name}/adclick-model-input/advertising.csv')
 
-X = df[df.columns.difference(['Class'])]
-y = df['Class']
+df['Country'] = encoder.fit_transform(df['Country'])
+df['City'] = encoder.fit_transform(df['City'])
+df.drop('Ad Topic Line', axis=1, inplace=True)
+
+df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+df['Hour'] = df['Timestamp'].dt.hour
+df['Minute'] = df['Timestamp'].dt.minute
+df['DayOfWeek'] = df['Timestamp'].dt.dayofweek
+df.drop('Timestamp', axis=1, inplace=True)
+
+X = df[df.columns.difference(['Clicked on Ad'])]
+y = df['Clicked on Ad']
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 rfc_model = RandomForestClassifier(n_estimators=600)
 rfc_model.fit(X_train,y_train)
